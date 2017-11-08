0894e47761ff152bbc7d82d81b547b2b38823fe3
koiti <steventsukamoto@gmail.com>
2017-11-08 17:33:56 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/steven/modelagel_conceitual/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.steven.modelagel_conceitual.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.steven.modelagel_conceitual.domain.Categoria;
+import com.steven.modelagel_conceitual.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+}
