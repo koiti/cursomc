af24361faf9c5f3d7eac0cc42a21d118e591cca7
koiti <steventsukamoto@gmail.com>
2017-11-08 17:49:42 -0200

    Criando operacao de instanciacao

--- src/main/java/com/steven/modelagel_conceitual/CursoModelagemConceitualApplication.java
+++ src/main/java/com/steven/modelagel_conceitual/CursoModelagemConceitualApplication.java
@@ -1,12 +1,31 @@
 package com.steven.modelagel_conceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoModelagemConceitualApplication {
+import com.steven.modelagel_conceitual.domain.Categoria;
+import com.steven.modelagel_conceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoModelagemConceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
