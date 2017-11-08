cbbf645c749b4b0bff05cdacc47cdf7f0a11f719
koiti <steventsukamoto@gmail.com>
2017-11-08 15:22:04 -0200

    Projeto criado

---
+++ src/main/java/com/steven/modelagel_conceitual/CursoModelagemConceitualApplication.java
@@ -0,0 +1,12 @@
+package com.steven.modelagel_conceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
+	}
+}
---
+++ src/test/java/com/steven/modelagel_conceitual/CursoModelagemConceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.steven.modelagel_conceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoModelagemConceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
