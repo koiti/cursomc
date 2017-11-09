2d4d698fc1cc79aab58f08fd40caf552ed67eaf9
koiti <steventsukamoto@gmail.com>
2017-11-09 12:19:30 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/steven/modelagel_conceitual/services/CategoriaService.java
+++ src/main/java/com/steven/modelagel_conceitual/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.steven.modelagel_conceitual.domain.Categoria;
 import com.steven.modelagel_conceitual.repositories.CategoriaRepository;
+import com.steven.modelagel_conceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto nao encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
