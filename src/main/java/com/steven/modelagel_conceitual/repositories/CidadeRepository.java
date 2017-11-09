0dd0ca077412dea8900d98cce06fe94a648aaf4a
koiti <steventsukamoto@gmail.com>
2017-11-09 14:34:55 -0200

    Estado e Cidade

---
+++ src/main/java/com/steven/modelagel_conceitual/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.steven.modelagel_conceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.steven.modelagel_conceitual.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
